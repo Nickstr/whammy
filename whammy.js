a1238759d6133b4d23fa518f3d3c6c569c05051f
Nick <nickstr@nickstr.nl>
2016-03-05 13:39:28 +0100

    Update whammy.js

--- whammy.js
+++ whammy.js
@@ -496,9 +496,7 @@ window.Whammy = (function(){
 	WhammyVideo.prototype.add = function(frame, duration){
 		if(typeof duration != 'undefined' && this.duration) throw "you can't pass a duration if the fps is set";
 		if(typeof duration == 'undefined' && !this.duration) throw "if you don't have the fps set, you need to have durations here.";
-		if(frame.canvas){ //CanvasRenderingContext2D
-			frame = frame.canvas;
-		}
+	
 		if(frame.toDataURL){
 			// frame = frame.toDataURL('image/webp', this.quality);
 			// quickly store image data so we don't block cpu. encode in compile method.
